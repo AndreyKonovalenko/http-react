02b633942538c5089f105be04e4ce245fe05f03c
Andrey <andreinew@mail.ru>
2018-07-10 09:30:02 +0000

    finished 192 lessen -switch example

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -34,8 +34,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                     <Route path="/" exact component={Posts} />
-                    <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" component={FullPost} />    
+                    <Switch>
+                        <Route path="/new-post" component={NewPost} />
+                        <Route path="/:id" component={FullPost} /> 
+                    </Switch>
                 </header>
             </div>
         );
