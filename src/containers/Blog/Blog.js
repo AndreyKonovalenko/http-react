9dce1475af65178113c43d26ec6382668841d3fb
Andrey <andreinew@mail.ru>
2018-07-11 14:49:58 +0000

    202 Loadin Routes Lazily:  this technique of downloading only what we need as know as splitting code or lazy loading, it works with create-react-app webpack config

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
 //import FullPost from './FullPost/FullPost';
-
-
+import asyncComponent from '../../hoc/asyncComponent';
 import './Blog.css';
+
+
+const AsycnNewPost = asyncComponent(() => {
+    //dynamic sytax, it means that import will execute only when anonymous () function will executed
+    // and the function will be executed once we rendered AscynNewPost component to the screen
+    return import('./NewPost/NewPost');
+}); 
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
     
     render () {
@@ -37,7 +43,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                     <Switch>
-                        {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                        {this.state.auth ? <Route path="/new-post" component={AsycnNewPost} /> : null}
                         <Route path="/posts" component={Posts} />
                         <Route render={() => <h1>Not found</h1>}/>
                         {/*<Redirect from="/" to="/posts" />*/}
